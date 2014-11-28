24b96ba63308f81014077d089c807f5f3afc46bb
TRONG LAM PHAN <trong-lam.phan@seitosei.fr>
2014-11-28 03:49:37 +0100

    update

---
+++ latency/Perf.java
@@ -0,0 +1,242 @@
+package latency;
+
+import java.util.concurrent.Semaphore;
+
+import trains.CallbackCircuitChange;
+import trains.CallbackUtoDeliver;
+import trains.CircuitView;
+import trains.Interface;
+import trains.Message;
+
+public class Perf {
+
+	// static boolean sender;
+	static int broadcasters;
+	static int delayBetweenTwoUtoBroadcast = 1; // millis
+	// static int nbRecMsgBeforeStop;
+	static int nbRecMsg = 0;
+	static boolean measurementDone;
+
+	static Semaphore semWaitEnoughMembers;
+
+	static Interface trin = Interface.trainsInterface();
+	static int rank;
+
+	public static class myCallbackCircuitChange implements
+			CallbackCircuitChange {
+
+		private static final myCallbackCircuitChange CIRCUITCHANGE = new myCallbackCircuitChange();
+		private int id = 0;
+
+		private myCallbackCircuitChange() {
+			// Nothing to do
+		}
+
+		public static myCallbackCircuitChange getInstance() {
+			return CIRCUITCHANGE;
+		}
+
+		public void setId(int id) {
+			this.id = id;
+		}
+
+		@Override
+		public void run(CircuitView cv) {
+			// Printing the circuit modification
+			System.out
+					.println("!!! ******** callbackCircuitChange called with "
+							+ cv.getMemb() + " members (process ");
+
+			// Printing the new/departed participant
+			if (cv.getJoined() != 0) {
+				System.out.println(Integer.toString(cv.getJoined())
+						+ " has arrived.)");
+			} else {
+				System.out.println(Integer.toString(cv.getDeparted())
+						+ " is gone.)");
+				if (!measurementDone) {
+					System.out
+							.println("!!! ******** Experience has failed ******** !!!\n");
+					return;
+				}
+			}
+
+			System.out.println(cv.getMemb() + " // " + broadcasters);
+
+			if (cv.getMemb() >= broadcasters) {
+				for (rank = 0; (rank < cv.getMemb())
+						&& trin.JgetMyAddress() != cv.getMembersAddress(rank); rank++)
+					;
+				System.out
+						.println("!!! ******** enough members to start utoBroadcasting\n");
+				semWaitEnoughMembers.release();
+			}
+		}
+	}
+
+	public static class myCallbackUtoDeliver implements CallbackUtoDeliver {
+
+		private static final myCallbackUtoDeliver UTODELIVER = new myCallbackUtoDeliver();
+
+		public myCallbackUtoDeliver() {
+			// Nothing to do
+		}
+
+		public static myCallbackUtoDeliver getInstance() {
+			return UTODELIVER;
+		}
+
+		@Override
+		public void run(int sender, Message msg) {
+
+			nbRecMsg++;
+
+			/*
+			 * String content = new String(msg.getPayload());
+			 * System.out.println("!!! " + nbRecMsg + "-ieme message (recu de "
+			 * + sender + " / contenu = " + content + ")");
+			 */
+		}
+	}
+
+	public static void main(String args[]) {
+
+		// Trying to use environment variables to get the values of the
+		// parameters for a use of a script for the perf tests
+
+		/*
+		 * String nomBroadcaster = System.getenv("BROADCASTERS"); String
+		 * nomWarmup = System.getenv("WARMUP"); String nomCooldown =
+		 * System.getenv("COOLDOWN"); String nomMeasurement =
+		 * System.getenv("MEASUREMENT"); String nomNumber =
+		 * System.getenv("NUMBER"); String nomSize = System.getenv("SIZE");
+		 * 
+		 * if(nomBroadcaster == null || nomWarmup == null || nomCooldown == null
+		 * || nomMeasurement == null || nomNumber == null || nomSize == null){
+		 * System.out.println("### Wrong parameters ! ###"); System.out.println(
+		 * "You need to set the following environment variables correctly : BROADCASTERS, WARMUP, COOLDOWN, MEASUREMENT, NUMBER, SIZE, NTRAINS"
+		 * ); System.exit(1); } else {
+		 * System.out.println("### Correct parameters ! ###"); }
+		 * 
+		 * broadcasters = Integer.parseInt(nomBroadcaster); int warmup =
+		 * Integer.parseInt(nomWarmup); int cooldown =
+		 * Integer.parseInt(nomCooldown); int measurement =
+		 * Integer.parseInt(nomMeasurement); int number =
+		 * Integer.parseInt(nomNumber); int size = Integer.parseInt(nomSize);
+		 */
+
+		// trInit parameters: values by default
+		int trainsNumber = 0;
+		int wagonLength = 0;
+		int waitNb = 0;
+		int waitTime = 0;
+
+		byte[] payload = null;
+		int rankMessage = 0;
+		Message msg = null;
+		int exitcode;
+		int i = 0;
+		int maxConcurrentRequests = 1;
+
+		rank = 0;
+
+		// Test parameters
+		broadcasters = 2;
+		int cooldown = 1; /* Default value = 10 seconds */
+		int measurement = 10; /* Default value = 600 seconds */
+		int number = 2;
+		int size = 10;
+		int warmup = 1; /* Default value = 300 seconds */
+		measurementDone = false;
+
+		// Semaphores initialization
+		semWaitEnoughMembers = new Semaphore(maxConcurrentRequests, true);
+
+		// Callback
+		myCallbackCircuitChange mycallbackCC = myCallbackCircuitChange
+				.getInstance();
+		mycallbackCC.setId(1);
+		myCallbackUtoDeliver mycallbackUto = myCallbackUtoDeliver.getInstance();
+
+		try {
+			semWaitEnoughMembers.acquire();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		// timeKeeper.setTimeLoadInterfaceBegins(System.nanoTime());
+		// timeKeeper.setTimeLoadInterfaceEnds(System.nanoTime());
+
+		TimeKeeper timeKeeper = new TimeKeeper.Builder(trin, broadcasters,
+				number, size).warmup(warmup).measurement(measurement)
+				.cooldown(cooldown).build();
+
+		timeKeeper.setTimeJtrInitBegins(System.nanoTime());
+
+		exitcode = trin.JtrInit(trainsNumber, wagonLength, waitNb, waitTime,
+				myCallbackCircuitChange.class.getName(),
+				myCallbackUtoDeliver.class.getName());
+
+		timeKeeper.setTimeJtrInitEnds(System.nanoTime());		
+
+		if (exitcode < 0) {
+			System.out.println("JtrInit failed.");
+			return;
+		}
+
+		try {
+			semWaitEnoughMembers.acquire();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		new Thread(timeKeeper).start();
+		
+		if (rank < broadcasters) {
+			while (!measurementDone) {
+
+				payload = Message.IntToByteArray(rankMessage);
+				if (payload == null) {
+					System.out
+							.println("Converting payload to byte array failed.");
+					return;
+				}
+
+				msg = Message.messageFromPayload(payload);
+
+				if (msg == null) {
+					System.out.println("Creating a message failed.");
+					return;
+				}
+
+				trin.Jnewmsg(msg.getPayload().length, msg.getPayload());
+
+				rankMessage++;
+
+				exitcode = trin.JutoBroadcast(msg);
+				if (exitcode < 0) {
+					System.out.println("JutoBroadcast failed.");
+					return;
+				}
+
+				try {
+					Thread.sleep(delayBetweenTwoUtoBroadcast);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+		System.out.println("rankMessage: " + rankMessage);
+		System.out.println("** JtrTerminate");
+		exitcode = trin.JtrTerminate();
+		if (exitcode < 0) {
+			System.out.println("JtrTerminate failed.");
+			return;
+		}
+
+		System.out.println("\n*********************\n");
+		// System.exit(0);
+	}
+}
---
+++ latency/TimeKeeper.java
@@ -0,0 +1,168 @@
+package latency;
+
+import java.util.Vector;
+
+import trains.Interface;
+
+public class TimeKeeper implements Runnable {
+	
+	private final Interface trin;
+	
+	private final int broadcasters;
+	private final int number;
+	private final int size;
+	private final int warmup;
+	private final int measurement;
+	private final int cooldown;
+	
+	private long timeLoadInterfaceBegins = -1;
+	private long timeLoadInterfaceEnds = -1;
+	
+	private long timeJtrInitBegins = -1;
+	private long timeJtrInitEnds = -1;	
+	
+	public static class Builder{
+		//Required parameters
+	    private final Interface trin;
+		private final int broadcasters;
+		private final int number;
+		private final int size;
+	
+		//Optional parameters - initialized with default values
+		private int warmup = 300;
+		private int measurement = 600;
+		private int cooldown = 10;
+		
+		public Builder(Interface trin, int broadcasters, int number, int size){
+			this.trin = trin;
+			this.broadcasters = broadcasters;
+			this.number = number;
+			this.size = size;
+		}
+		
+		public Builder warmup(int val){
+			warmup = val;
+			return this;
+		}
+
+		public Builder measurement(int val){
+			measurement = val;
+			return this;
+		}
+		
+		public Builder cooldown(int val){
+			cooldown = val;
+			return this;
+		}		
+		public TimeKeeper build(){
+			return new TimeKeeper(this);
+		}
+	}
+	
+	private TimeKeeper(Builder builder){
+		trin = builder.trin;
+		broadcasters = builder.broadcasters;
+		number = builder.number;
+		size = builder.size;
+		warmup = builder.warmup;
+		measurement = builder.measurement;
+		cooldown = builder.warmup;
+	}
+	
+	public void run(){
+
+		/* What we want: 
+		 * time for JtrInit
+		 * elapsed time
+		 * ru_utime
+		 * su-utime
+		 * number of messages delivered by the application
+		 * number of bytes delivered to the application
+		 * number of calls to newmsg() 
+		 * and more... */
+
+		long timeBegins = -1;
+		long timeEnds = -1;
+		
+		InterfaceJNI perfin = InterfaceJNI.perfInterface();
+		
+		try {
+			//System.out.println("warmup");
+			Thread.sleep(this.warmup * 1000);
+			//System.out.println("timeBegins");
+
+			timeBegins = System.nanoTime();
+			//System.out.println("getrusageBegin");
+			perfin.JgetrusageBegin();
+			//System.out.println("JsetcountersBegin");
+			perfin.JsetcountersBegin(trin);
+			//System.out.println("measurment");
+			
+			Thread.sleep(this.measurement * 1000);
+			
+			timeEnds = System.nanoTime();
+			perfin.JgetrusageEnd();
+			perfin.JsetcountersEnd(trin);
+			
+			Thread.sleep(this.cooldown * 1000);
+			
+			this.setMeasurementDone();
+
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		System.out.println("Time for LoadInterface (in sec): " + (this.timeLoadInterfaceEnds - this.timeLoadInterfaceBegins)/1000000000d);
+		System.out.println("Time for JtrInit (in sec): " + (this.timeJtrInitEnds - this.timeJtrInitBegins)/1000000000d);
+		System.out.println("Elasped time (in sec): " + (timeEnds - timeBegins)/1000000000d);
+		
+		System.out.println("ru_utime: " + perfin.Jgetru_utime()/1000000d);
+		System.out.println("su_utime: " + perfin.Jgetru_stime()/1000000d);
+
+		System.out.println("number of messages delivered to the application: " + perfin.Jgetmessages_delivered());
+		System.out.println("number of bytes delivered to the application: " + perfin.Jgetmessages_bytes_delivered());
+		System.out.println("number of bytes of trains received from the network: " + perfin.Jgettrains_bytes_received());
+		System.out.println("number of trains received from the network: " + perfin.Jgettrains_received());
+		System.out.println("number of bytes of recent trains received from the network: " + perfin.Jgetrecent_trains_bytes_received());
+		System.out.println("number of recent trains received from the network: " + perfin.Jgetrecent_trains_received());
+		System.out.println("number of wagons delivered to the application: " + perfin.Jgetwagons_delivered());
+		System.out.println("number of times automaton has been in state WAIT: " + perfin.Jgetwait_states());
+		System.out.println("number of calls to commRead() : " + perfin.Jgetcomm_read());
+		System.out.println("number of bytes read by commRead() calls: " + perfin.Jgetcomm_read_bytes());
+		System.out.println("number of calls to commReadFully(): " + perfin.Jgetcomm_readFully());
+		System.out.println("number of bytes read by commReadFully() calls: " + perfin.Jgetcomm_readFully_bytes());
+		System.out.println("number of calls to commWrite(): " + perfin.Jgetcomm_write());
+		System.out.println("number of bytes written by commWrite() calls: " + perfin.Jgetcomm_write_bytes());
+		System.out.println("number of calls to commWritev(): " + perfin.Jgetcomm_writev());
+		System.out.println("number of bytes written by commWritev() calls: " + perfin.Jgetcomm_writev_bytes());
+		System.out.println("number of calls to newmsg(): " + perfin.Jgetnewmsg());
+		System.out.println("number of times there was flow control when calling newmsg(): " + perfin.JgetflowControl());
+		
+		System.out.println("\nBroadcasters / number / size / ntr / Average number of delivered wagons per recent train received / Average number of msg per wagon / Throughput of uto-broadcasts in Mbps ; "+broadcasters+" ; "+number+" ; "+size+" ; ?ntr ; "+perfin.Jgetwagons_delivered()/perfin.Jgetrecent_trains_received()+" ; "+perfin.Jgetmessages_delivered()/perfin.Jgetwagons_delivered()+" ; "+perfin.Jgetmessages_bytes_delivered()/((timeEnds - timeBegins)/1000000000d)+"\n");
+
+		System.exit(0);
+	}
+	
+	public void setTimeLoadInterfaceBegins(long val){
+		this.timeLoadInterfaceBegins = val;
+	}
+	
+	public void setTimeLoadInterfaceEnds(long val){
+		this.timeLoadInterfaceEnds = val;
+	}
+	
+	public void setTimeJtrInitBegins(long val){
+		this.timeJtrInitBegins = val;
+	}
+	
+	public void setTimeJtrInitEnds(long val){
+		this.timeJtrInitEnds = val;
+	}
+	
+	public void setMeasurementDone(){
+		Perf.measurementDone = true;
+	}
+
+}
+
