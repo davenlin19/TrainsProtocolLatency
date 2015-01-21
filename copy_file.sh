8ca37619533b220087c42b393fc1d98e67075a96
TRONG LAM PHAN <trong-lam.phan@seitosei.fr>
2015-01-21 18:13:41 +0100

    update

--- copy_file.sh
+++ copy_file.sh
@@ -1,6 +1,7 @@
 cp ~/TrainsProtocolLatency/TrainsProtocolJava/src/perf/*.java src/perf/
 cp ~/TrainsProtocolLatency/TrainsProtocolJava/src/trains/*.java src/trains/
-cp ~/TrainsProtocolLatency/TrainsProtocolJava/src/twitter/*.java src/twitter/
+cp -rf ~/TrainsProtocolLatency/TrainsProtocolJava/src/twitter src/
+cp -rf ~/TrainsProtocolLatency/TrainsProtocolJava/src/latency src/
 cp ~/TrainsProtocolLatency/TrainsProtocolJava/addr_file ./
 cp -rf ~/TrainsProtocolLatency/TrainsProtocolJava/bin ./
 cp ~/TrainsProtocolLatency/TrainsProtocolJava/build.xml ./
